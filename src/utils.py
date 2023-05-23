a6e2749ee863340e027b4cb099c4755b333ed999
Carlos Nunez <carlos_nunez@berkeley.edu>
2023-05-22 23:17:28 -0700

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -17,7 +17,8 @@ def save_object(file_path, obj):
             dill.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e, sys)
-    
+
+
 def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
@@ -39,3 +40,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     
     except Exception as e:
         CustomException(e,sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e,sys)
