282105bb321b309359ccab22c6267070b224a6ef
Carlos Nunez <carlos_nunez@berkeley.edu>
2023-05-23 19:52:38 -0700

    From app to application

--- application.py
+++ application.py
@@ -4,7 +4,8 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-app = Flask(__name__)
+application = Flask(__name__)
+app = application
 
 @app.route('/')
 def index():
@@ -31,4 +32,4 @@ def predict_datapoint():
     return render_template('home.html', results = results[0])
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")      
+    app.run(host="0.0.0.0") 
